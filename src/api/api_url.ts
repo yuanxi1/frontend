8615d69bf8ae1a46e7ecb87f7ea39c0a00ddb499
Yuanxi Zhu <yuanxizhu@YXMacBook-Air.local>
2022-01-23 15:28:42 +0800

    correct API_URL

--- src/api/api_url.ts
+++ src/api/api_url.ts
@@ -1,2 +1,2 @@
 // export const API_URL = "http://localhost:8000/api/v1/";
-export const API_URL = "https://git.heroku.com/simplydone.git/"
+export const API_URL = "https://simplydone.herokuapp.com/api/v1/"
